ffbe071a3a60ba3da341f72bc8eea5fb5830f5e7
Manuel Herdez <herdez@gmail.com>
2022-04-07 22:31:51 -0500

    ajuste tiempo a 7 cifras

--- src/components/Horas.jsx
+++ src/components/Horas.jsx
@@ -14,11 +14,25 @@ function Horas({ hrs, handdleChange }) {
 				className='input-range'
 			/>
 			<div className='horario-seleccion-horas'>
-				<p className='horas hora2'>{hrs - 2 >= 0 ? hrs - 2 : null}</p>
-				<p className='horas hora1'>{hrs - 1 >= 0 ? hrs - 1 : null}</p>
+				<p className='horas hora3'>
+					{hrs - 3 >= 0 ? hrs - 3 : null}
+				</p>
+				<p className='horas hora2'>
+					{hrs - 2 >= 0 ? hrs - 2 : null}
+				</p>
+				<p className='horas hora1'>
+					{hrs - 1 >= 0 ? hrs - 1 : null}
+				</p>
 				<p className='horas hora0'>{hrs}</p>
-				<p className='horas hora1'>{hrs + 1 <= 23 ? hrs + 1 : null}</p>
-				<p className='horas hora2'>{hrs + 2 <= 23 ? hrs + 2 : null}</p>
+				<p className='horas hora1'>
+					{hrs + 1 <= 23 ? hrs + 1 : null}
+				</p>
+				<p className='horas hora2'>
+					{hrs + 2 <= 23 ? hrs + 2 : null}
+				</p>
+				<p className='horas hora3'>
+					{hrs + 3 <= 23 ? hrs + 3 : null}
+				</p>
 			</div>
 		</div>
 	);
--- src/components/Minutos.jsx
+++ src/components/Minutos.jsx
@@ -4,11 +4,25 @@ function Minutos({ min, handdleChange }) {
 	return (
 		<div>
 			<div className='horario-seleccion-minutos'>
-				<p className='minutos minuto2'>{min - 2 >= 0 ? min - 2 : null}</p>
-				<p className='minutos minuto1'>{min - 1 >= 0 ? min - 1 : null}</p>
+				<p className='minutos minuto3'>
+					{min - 3 >= 0 ? min - 3 : null}
+				</p>
+				<p className='minutos minuto2'>
+					{min - 2 >= 0 ? min - 2 : null}
+				</p>
+				<p className='minutos minuto1'>
+					{min - 1 >= 0 ? min - 1 : null}
+				</p>
 				<p className='minutos minuto0'>{min}</p>
-				<p className='minutos minuto1'>{min + 1 <= 59 ? min + 1 : null}</p>
-				<p className='minutos minuto2'>{min + 2 <= 59 ? min + 2 : null}</p>
+				<p className='minutos minuto1'>
+					{min + 1 <= 59 ? min + 1 : null}
+				</p>
+				<p className='minutos minuto2'>
+					{min + 2 <= 59 ? min + 2 : null}
+				</p>
+				<p className='minutos minuto3'>
+					{min + 3 <= 59 ? min + 3 : null}
+				</p>
 			</div>
 			<input
 				type='range'
